a25fabb72150e9f4e9486723ae043333f58fd2b6
Satenik-M <satenik@markosyan.am>
2016-04-26 11:49:05 +0400

    icons added, print media completed, project finalized

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-var navList = document.getElementsByClassName("nav");
-for (var i = 0; i<navList.length i++) {
-	navList[i].addEventListener("click", function(){this.style.backgroundColor = "#FFFDF6";});
-}
+$(document).ready(function () {
+    
+})
+
+
+
+
+
